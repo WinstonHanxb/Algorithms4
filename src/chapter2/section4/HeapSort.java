98bbeff173d50dfd4871b38013deaf24d3c92077
WinstonHan <winstonhan@bupt.edu.cn>
2017-11-22 10:14:50 +0800

    priority queue algorithm

--- src/chapter2/section4/HeapSort.java
+++ src/chapter2/section4/HeapSort.java
@@ -7,6 +7,11 @@ public class HeapSort {
 
     public static void sort(Comparable[] a) {
         int size = a.length;
+        //堆排序建堆的过程
+        //虽然也可以从左往右遍历数组，但是简便的把整个数组理解成为一个没排序的堆
+        //因此从右往左保证一个个子树是有序的，最终一定能保证整个数组是有序的
+        //由二叉树的性质，size/2往后的一定全部都是叶节点，或者说是只有一个节点的子数，必然是有序的
+        //因此从size/2往左排序，将所有的不只一个节点的子树整理有序即完成了建堆
         for (int i = size / 2; i > 0; i--) {
             sink(a, i, size);
         }
